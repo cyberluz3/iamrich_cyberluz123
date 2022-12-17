6d5fc843792b7aee4c51ce979781e8ac037e25bb
cyberluzparayno8@gmail.com <cyberluzparayno8@gmail.com>
2022-12-17 13:01:52 +0800

    first commit

---
+++ android/app/src/main/kotlin/com/example/iamerich_ycy/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.iamerich_ycy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('I Am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('image/d.jpg'),
+          ),
+        ),
+      )));
+}
